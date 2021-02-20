e84195ceedd104852caa13fe4559ec2d8e733da8
prabhu1122 <prabhat199541@gmail.com>
2021-02-20 20:41:19 +0530

    add some

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var wkr = [];
-var boxLength = 50;
+var boxLength = 80;
 var dis;
 var radius = 5;
 var startDis = 50;
@@ -15,11 +15,11 @@ function setup() {
 
 function draw() {
     // body...
-    background(220);
+    background(255);
     for (var particals = 0; particals < wkr.length; particals++) {
-        for (var other = 0; other < wkr.length; other++) {
+        for (var other = particals; other < wkr.length; other++) {
             let d = wkr[particals].checkDist(wkr[other]);
-            if (particals !== other && d < startDis) {
+            if (d < startDis) {
                 if (d <= 2 * radius) {
                     wkr[particals].resolveCollision(wkr[other]);
                 }
@@ -29,9 +29,10 @@ function draw() {
         wkr[particals].show();
         wkr[particals].update();
     }
-    if (random(1) < 0.3 && mouseX >= radius && mouseX <= width-radius) {
+    if (random(1) < 0.01 && mouseX >= radius && mouseX <= width-radius) {
         if (mouseIsPressed) {
             //wkr.push(new walker(mouseX, mouseY, startDis, radius));
         }
+        //wkr.push(new walker(mouseX, mouseY, startDis, radius));
     }
 }
